3129fcb371a5f18738a9387e66f6ac8f7f900eb9
Siddhanth Dwivedi <hackersiddhant@hotmail.com>
2019-07-25 15:05:28 +0530

    update

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -1,25 +1,13 @@
-/**
- * Copyright (c) 2017-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
-// See https://docusaurus.io/docs/site-config for all the possible
-// site configuration options.
 
 // List of projects/orgs using your project for the users page.
 const users = [
-];
-
+];  
 const siteConfig = {
   title: 'Exploit Guide', // Title for your website.
   tagline: 'Security Blog',
   url: 'https://securityheaven.ml', // Your website URL
   baseUrl: '/', // Base URL for your project */
-  // For github.io type URLs, you would set the url and baseUrl like:
-  //   url: 'https://facebook.github.io',
-  //   baseUrl: '/test-site/',
+  
 
   // Used for publishing and more
   projectName: 'securityblog',
@@ -76,6 +64,7 @@ const siteConfig = {
 
   // Add custom scripts here that would be placed in <script> tags.
   scripts: ['https://buttons.github.io/buttons.js'],
+
 
   // On page navigation for the current documentation page.
   onPageNav: 'separate',
